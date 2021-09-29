5075f8fe8d60a2d0534ddb1555c0c1b0f94abce4
maxinth <tunde.j.adeleye@gmail.com>
2021-09-29 15:05:39 +0100

    initial cleanup

---
+++ src/AppStyled.js
@@ -0,0 +1,64 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+ 
+}
+:root{
+    --gradientStart: #37b9ca;
+    --gradientEnd: #217079;
+    --themeDarkColor: #1c5c64;
+    --mainTeal:#37b9ca;
+    --blue: #007bff;
+    --indigo: #6610f2;
+    --purple: #6f42c1;
+    --pink: #e83e8c;
+    --red: #dc3545;
+    --orange: #fd7e14;
+    --yellow: #ffc107;
+    --green: #28a745;
+    --teal: #20c997;
+    --cyan: #17a2b8;
+    --white: #fff;
+    --gray: #6c757d;
+    --gray-dark: #343a40;
+    --primary: #007bff;
+    --secondary: #6c757d;
+    --success: #28a745;
+    --info: #17a2b8;
+    --warning: #ffc107;
+    --danger: #dc3545;
+    --light: #f8f9fa;
+    --dark: #343a40;
+    --breakpoint-xs: 0;
+    --breakpoint-sm: 576px;
+    --breakpoint-md: 768px;
+    --breakpoint-lg: 992px;
+    --breakpoint-xl: 1200px;
+    --font-family-sans-serif: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+    --font-family-monospace: SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;
+}
+body, #root{
+    width:100%;
+    height:100%;
+}
+a{
+    text-decoration:none;
+}
+li,ul{
+    list-style-type:none;
+}
+`;
+
+const AppBox = styled.div`
+  width: 100%;
+  height: 100%;
+  /* max-width: 1400px; */
+  overflow: hidden;
+  font-family: "Open sans", sans-serif;
+`;
+
+export { GlobalStyle, AppBox };
